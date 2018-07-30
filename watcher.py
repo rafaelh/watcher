e2321bd0a0f5acf110fa35f4dd5516bf6d78f3b1
Rafael Hart <rafael.hart9@gmail.com>
2018-07-30 16:50:11 +1000

    Main script

---
+++ watcher.py
@@ -0,0 +1,54 @@
+""" A simple RSS feed watcher """
+
+import sqlite3
+import smtplib
+from email.mime.text import MIMEText
+
+import feedparser
+
+DATABASE = '/var/tmp/watcher_rss.sqlite'
+FEED = 'https://xkcd.com/rss.xml'
+
+db_connection = sqlite3.connect(DATABASE)
+db = db_connection.cursor()
+db.execute('CREATE TABLE IF NOT EXISTS website (title TEXT, date TEXT)')
+
+def article_is_not_db(article_title, article_date):
+    """ Check if a given pair of article title and date is in the database
+    Args:
+        article_title (str): The title of an article
+        article_date  (str): The publication date of an article
+    Return:
+        True if the article is not in the database
+        False if the article is already present in the database
+    """
+    db.execute("SELECT * from website WHERE title=? AND date=?", (article_title, article_date))
+    if not db.fetchall():
+        return True
+    else:
+        return False
+
+def add_article_to_db(article_title, article_date):
+    """ Add a new article title and date to the database
+    Args:
+        article_title (str): The title of an article
+        article_date (str): The publication date of an article
+    """
+    db.execute("INSERT INTO website VALUES (?,?)", (article_title, article_date))
+    db_connection.commit()
+
+def send_notification(article_title, article_url):
+
+    print("Hi there is a new website article:" + article_title + ".\n You can read it here " + article_url)
+
+def read_article_feed():
+    """ Get articles from RSS feed """
+    feed = feedparser.parse(FEED)
+    for article in feed['entries']:
+        if article_is_not_db(article['title'], article['published']):
+            send_notification(article['title'], article['link'])
+            add_article_to_db(article['title'], article['published'])
+
+if __name__ == '__main__':
+    read_article_feed()
+    db_connection.close()
